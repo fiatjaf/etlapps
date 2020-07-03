2f74921ef12ff165626f6ed3b8cabf07bac86dbb
fiatjaf <fiatjaf@alhur.es>
2020-07-03 01:27:59 -0300

    lichess: initial version (test).

---
+++ lichess/src/helpers.js
@@ -0,0 +1,23 @@
+import shajs from 'sha.js'
+import {writable} from 'svelte/store'
+
+export const state = writable({})
+
+export function sha256(v) {
+  return shajs('sha256')
+    .update(v)
+    .digest('hex')
+}
+
+export function getBaseSecret() {
+  var baseSecret = localStorage.getItem('secret')
+  if (!baseSecret) {
+    baseSecret = parseInt(Math.random() * 99999999)
+    localStorage.setItem('secret', baseSecret)
+  }
+  return baseSecret
+}
+
+export function capitalize(word) {
+  return word[0].toUpperCase() + word.slice(1)
+}
