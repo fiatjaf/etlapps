3bd3db50075a11486f90817f43acdb16bcda5feb
fiatjaf <fiatjaf@alhur.es>
2020-07-12 15:17:35 -0300

    chainmarket: prompt to handle bitcoin: links.

--- chainmarket/src/main.js
+++ chainmarket/src/main.js
@@ -7,3 +7,15 @@ const app = new App({
 })
 
 export default app
+
+setTimeout(() => {
+  try {
+    navigator.registerProtocolHandler(
+      'bitcoin',
+      'https://chainmarket.etleneum.com/#addr=%s',
+      'ChainMarket'
+    )
+  } catch (err) {
+    console.log('registerProtocolHandler:', err)
+  }
+}, 100000)
