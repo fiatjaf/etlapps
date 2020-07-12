106c45b6faf8e803f2c391232b7900504dceecb4
fiatjaf <fiatjaf@alhur.es>
2020-07-12 15:30:05 -0300

    chainmarket: just ask the protocol handler thing immediately.

--- chainmarket/src/main.js
+++ chainmarket/src/main.js
@@ -8,14 +8,12 @@ const app = new App({
 
 export default app
 
-setTimeout(() => {
-  try {
-    navigator.registerProtocolHandler(
-      'bitcoin',
-      'https://chainmarket.etleneum.com/#addr=%s',
-      'ChainMarket'
-    )
-  } catch (err) {
-    console.log('registerProtocolHandler:', err)
-  }
-}, 100000)
+try {
+  navigator.registerProtocolHandler(
+    'bitcoin',
+    'https://chainmarket.etleneum.com/#addr=%s',
+    'ChainMarket'
+  )
+} catch (err) {
+  console.log('registerProtocolHandler:', err)
+}
