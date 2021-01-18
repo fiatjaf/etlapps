4fcdb51de12abe9f89312d05371b379549792675
Tiago Vasconcelos <tvasconcelos@gmail.com>
2021-01-18 19:25:36 +0000

    smt new website first iteration

--- smt/rollup.config.js
+++ smt/rollup.config.js
@@ -2,11 +2,38 @@
 
 import json from 'rollup-plugin-json'
 import svelte from 'rollup-plugin-svelte'
+import builtins from 'rollup-plugin-node-builtins'
+import globals from 'rollup-plugin-node-globals'
 import resolve from 'rollup-plugin-node-resolve'
 import commonjs from 'rollup-plugin-commonjs'
 import {terser} from 'rollup-plugin-terser'
+import css from 'rollup-plugin-css-only';
 
-const production = !!process.env.PRODUCTION
+import livereload from 'rollup-plugin-livereload';
+
+// const production = !!process.env.PRODUCTION
+const production = !process.env.ROLLUP_WATCH;
+
+function serve() {
+	let server;
+
+	function toExit() {
+		if (server) server.kill(0);
+	}
+
+	return {
+		writeBundle() {
+			if (server) return;
+			server = require('child_process').spawn('npm', ['run', 'start', '--', '--dev'], {
+				stdio: ['ignore', 'inherit', 'inherit'],
+				shell: true
+			});
+
+			process.on('SIGTERM', toExit);
+			process.on('exit', toExit);
+		}
+	};
+}
 
 export default {
   input: 'src/main.js',
@@ -18,22 +45,25 @@ export default {
   },
   plugins: [
     json(),
-
     svelte({
-      // enable run-time checks when not in production
-      dev: !production,
-      // we'll extract any component CSS out into
-      // a separate file — better for performance
-      css: css => {
-        css.write('static/bundle.css')
-      }
-    }),
+			compilerOptions: {
+				// enable run-time checks when not in production
+				dev: !production
+			}
+		}),
+    css({ output: 'bundle.css' }),
 
     // If you have external dependencies installed from
     // npm, you'll most likely need these plugins. In
     // some cases you'll need additional configuration —
     // consult the documentation for details:
     // https://github.com/rollup/rollup-plugin-commonjs
+    resolve({
+			browser: true,
+			dedupe: ['svelte']
+		}),
+		builtins(),
+		globals(),
     resolve({
       browser: true,
       dedupe: importee =>
@@ -43,6 +73,14 @@ export default {
     commonjs({
       ignore: ['crypto']
     }),
+
+    // In dev mode, call `npm run start` once
+		// the bundle has been generated
+		!production && serve(),
+
+		// Watch the `public` directory and refresh the
+		// browser on changes when not in production
+		!production && livereload('static'),
 
     // If we're building for production (npm run build
     // instead of npm run dev), minify
---
+++ smt/src/App.svelte
@@ -0,0 +1,28 @@
+<script>
+  import {setContext} from 'svelte'
+  import Router from 'svelte-spa-router'
+  import {Contract} from 'etleneum'
+  import Index from './Index.svelte'
+  import Offer from './Offer.svelte'
+  import AccountPage from './AccountPage.svelte'
+  import Foot from './Footer.svelte'
+  
+  import kbAccountStore from '../../components/kbAccountStore'
+
+  const CONTRACT = 'c4g4e71a5g'
+
+  const routes = {
+    '/': Index,
+    '/account': AccountPage,
+    '/:offer_id': Offer
+  }
+
+  setContext('contract', Contract(CONTRACT))
+  setContext('kbaccounts', kbAccountStore)
+
+</script>
+
+<main>
+  <Router {routes} />
+</main>
+<Foot name="Reverse Crowdfunding" contract="{CONTRACT}" />
