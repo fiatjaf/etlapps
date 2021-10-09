ea8bf6accfde4c1011b6b46afee35f1c81cc5fd0
Saul <saulfreeberty@protonmail.com>
2021-10-09 19:43:02 +0300

    auction: show only toggled auction
    - show only toggled auction + top up account
    - show only create auction block if toggled + top up account

--- auction/src/Index.svelte
+++ auction/src/Index.svelte
@@ -23,12 +23,22 @@
   function auctionAppeared(element, id) {
     let hash = location.hash.slice(1)
     let [_, selected] = hash.split('-')
-    if (id === selected) element.open = true
+    if(selected){
+      if (id === selected){ element.open = true }
+      else{ element.style.display = 'none' }
+    }
+    if(hash == 'create_auction'){
+      var create_auction_wrapper = document.getElementById('create_auction_wrapper')
+      create_auction_wrapper.open = true
+      createAuctionToggled(create_auction_wrapper)
+    }
   }
 
   let toggling = false
   function auctionToggled(element, id) {
     if (toggling) return
+
+    var create_auction_wrapper = document.getElementById('create_auction_wrapper')
 
     if (element.open) {
       location.hash = `auction-${id}`
@@ -36,10 +46,14 @@
       // close all others
       toggling = true
       let auctionDetails = document.querySelectorAll('details.auction')
+
       for (let i = 0; i < auctionDetails.length; i++) {
-        let d = auctionDetails[i]
-        if (d === element) continue
-        d.open = false
+        if (auctionDetails[i] === element) continue
+        else{
+          auctionDetails[i].open = false
+          auctionDetails[i].style.display = 'none'
+          create_auction_wrapper.style.display = 'none'
+        }
       }
       toggling = false
     } else {
@@ -47,12 +61,42 @@
       setTimeout(() => {
         let auctionDetails = document.querySelectorAll('details.auction')
         for (let i = 0; i < auctionDetails.length; i++) {
+          auctionDetails[i].style.display = 'block'
+          create_auction_wrapper.style.display = 'block'
           if (auctionDetails[i].open) return
         }
         location.hash = ''
       }, 10)
     }
   }
+  function createAuctionToggled(element) {
+    if (toggling) return
+
+    if (element.open) {
+      location.hash = `create_auction`
+      // close all others
+      toggling = true
+      let auctionDetails = document.querySelectorAll('details.auction')
+
+      for (let i = 0; i < auctionDetails.length; i++) {
+          auctionDetails[i].open = false
+          auctionDetails[i].style.display = 'none'
+      }
+      toggling = false
+    } else {
+      // show all others the hash if none other is open
+      setTimeout(() => {
+        let auctionDetails = document.querySelectorAll('details.auction')
+        for (let i = 0; i < auctionDetails.length; i++) {
+          auctionDetails[i].style.display = 'block'
+          if (auctionDetails[i].open) return
+        }
+        location.hash = ''
+      }, 10)
+    }
+  }
+
+
 </script>
 
   {#each auctionList as auction (auction.id)}
@@ -71,11 +115,11 @@
     <p>No active auctions right now.</p>
   {/each}
   <hr />
-  <details>
+  <details id="create_auction_wrapper" on:toggle={ev => createAuctionToggled(ev.target)}>
     <summary> Create a new auction </summary>
     <Create />
   </details>
-  <details>
+  <details id="topup_account_wrapper">
     <summary> Top-up account balance </summary>
     <Topup />
   </details>
