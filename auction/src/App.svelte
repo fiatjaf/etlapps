71ef0948c9ba2016b112a222e1973bf70c417723
Saul <saulfreeberty@protonmail.com>
2021-10-18 16:35:55 +0300

    auction: finish auction, set alias, state filter

--- auction/src/App.svelte
+++ auction/src/App.svelte
@@ -12,13 +12,22 @@
   import Foot from '../../components/Footer.svelte'
 
   const CONTRACT = 'c2w3fm9zhaz'
+  const ALIAS_CONTRACT = 'c8g2k1bzcb' 
 
   const contract = Contract(CONTRACT)
+  const alias_contract = Contract(ALIAS_CONTRACT)
+
   setContext('contract', contract)
+  setContext('alias_contract', alias_contract)
 
   const auctionsStore = writable({})
+  const aliasesStore = writable({})
+
   setContext('auctions', auctionsStore)
+  setContext('aliases', aliasesStore)
+
   onMount(loadAuctions)
+  onMount(loadAliases)
 
   const EventSource = window.EventSource
   const ETLENEUM = window.etleneum || 'https://etleneum.com'
@@ -33,7 +42,10 @@
 
   async function loadAuctions() {
     try {
-      var auctions = await contract.state()
+      var yesterday_time = new Date()
+          yesterday_time.setDate(yesterday_time.getDate() - 1)
+      var yesterday_timestamp = Math.round(yesterday_time.getTime() / 1000)
+      var auctions = await contract.state(`[. | to_entries[] | {"key": .key, "value": .value |  select(.end_datetime > ${yesterday_timestamp} )}] | from_entries`)
       if (Array.isArray(auctions)) {
         auctions = {}
       }
@@ -49,7 +61,6 @@
         loadAuctions()
 
         let newCall = await contract.loadCall(id)
-
         if ($state.call.id === id) {
           for (let i = 0; i < unsetCallListeners.length; i++) {
             unsetCallListeners[i]()
@@ -124,6 +135,48 @@
       })
     }
   })
+
+  async function loadAliases() {
+    try {
+      var aliases = await alias_contract.state() // .state(`.["${$account.id}"]`)
+      if (Array.isArray(aliases)) {
+        aliases = {}
+      }
+      aliasesStore.set(aliases)
+    } catch (err) {
+      toast.error(`Error loading contract: ${err}`)
+    }
+  }
+
+ onMount(() => {
+    return alias_contract.stream(
+      async id => {
+        loadAliases()
+
+        let newCall = await alias_contract.loadCall(id)
+        if ($state.call.id === id) {
+          for (let i = 0; i < unsetCallListeners.length; i++) {
+            unsetCallListeners[i]()
+          }
+
+          switch (newCall.method) {
+            case 'add':
+              toast.success(`Your alias was added!`)
+              break
+            case 'delete':
+              toast.success(`Alias removed!`)
+              break
+          }
+        }
+      },
+      (id, errMessage) => {
+        if ($state.call === id) {
+          toast.warning(`Your call failed: ${errMessage}`)
+        }
+      }
+    )
+ })
+
 </script>
 
 <main>
